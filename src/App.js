923e275a750152f59dc0809ad18cb22e62408f7c
Halyna-K <hkzlvs@gmail.com>
2021-10-25 14:40:23 +0300

    change with useState

--- src/App.js
+++ src/App.js
@@ -1,80 +1,62 @@
 import coffeeToGo from "./logo.svg";
 import "./App.css";
-import { Component } from "react";
+import { useState } from "react";
 import { Title } from "./components/Title/Title";
 import { FeedbackOptions } from "./components/FeedbackOptions/FeedbackOptions";
 import { Notification } from "./components/Notification/Notification";
 import { Statistics } from "./components/Statistics/Statistics";
 
-export class App extends Component {
-  static defaultProps = {
-    initialGood: 0,
-    initialNeutral: 0,
-    initialBad: 0,
-    initialTotal: 0,
-    initialPositivePercentage: 0,
-  };
-  state = {
-    good: this.props.initialGood,
-    neutral: this.props.initialNeutral,
-    bad: this.props.initialBad,
-  };
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onLeaveFeedback = (e) => {
+  const objKey = Object.keys({ good, neutral, bad });
+
+  const onLeaveFeedback = (e) => {
     e.preventDefault();
-    const name = e.target.name;
-
-    this.setState((prevState) => ({
-      [name]: prevState[name] + 1,
-    }));
+    switch (e.target.name) {
+      case "good":
+        setGood((prevGood) => prevGood + 1);
+        break;
+      case "neutral":
+        setNeutral((prevNeutral) => prevNeutral + 1);
+        break;
+      case "bad":
+        setBad((prevBad) => prevBad + 1);
+        break;
+      default:
+    }
   };
 
-  onCountTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const onCountTotalFeedback = () => good + neutral + bad;
 
-    const total = good + neutral + bad;
-    return total;
-  };
+  const onCountPositivePercentage = () =>
+    Math.round(((good + neutral) * 100) / onCountTotalFeedback());
 
-  onCountPositivePercentage = () => {
-    const { good, neutral, bad } = this.state;
+  return (
+    <div className="App">
+      <img src={coffeeToGo} className="App-logo" alt="coffeeToGo" />
 
-    const positivePercentage = Math.round(
-      ((good + neutral) * 100) / (good + neutral + bad)
-    );
-    return positivePercentage;
-  };
+      <Title title="Please leave feedback">
+        <FeedbackOptions options={objKey} onLeaveFeedback={onLeaveFeedback} />
+      </Title>
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const { onLeaveFeedback, onCountTotalFeedback, onCountPositivePercentage } =
-      this;
-    const objKey = Object.keys(this.state);
-
-    return (
-      <div className="App">
-        <img src={coffeeToGo} className="App-logo" alt="coffeeToGo" />
-
-        <Title title="Please leave feedback">
-          <FeedbackOptions options={objKey} onLeaveFeedback={onLeaveFeedback} />
+      {onCountTotalFeedback(objKey) === 0 ? (
+        <Notification message="No feedback given" />
+      ) : (
+        <Title title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={onCountTotalFeedback(objKey)}
+            positivePercentage={onCountPositivePercentage(objKey)}
+          />
         </Title>
-
-        {onCountTotalFeedback(objKey) === 0 ? (
-          <Notification message="No feedback given" />
-        ) : (
-          <Title title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={onCountTotalFeedback(objKey)}
-              positivePercentage={onCountPositivePercentage(objKey)}
-            />
-          </Title>
-        )}
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
 
 export default App;
